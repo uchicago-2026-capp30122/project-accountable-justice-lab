aa27327565e1f6d4ea4eeca42d8afc133031ad5a
Jimena <jimenagomez@uchicago.edu>
2026-03-07 18:32:07 -0600

    fixed paths

--- src/cleaning_and_processing/solicitudes/ngrams_datos.py
+++ src/cleaning_and_processing/solicitudes/ngrams_datos.py
@@ -1,16 +1,29 @@
 import pandas as pd
 import csv
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[3]
+
+
+sys.path.append(str(ROOT / "src" / "analysis" / "solicitudes"))
+sys.path.append(str(ROOT / "src" / "cleaning_and_processing" / "solicitudes"))
+
 from salient_tokens_solicitudes import analyze_themes
 from conteo_ministros_sol import INTEGRACION_POR_ANIO
 
+
+
 def main():
-    csv_path = "clean_output/clean_solicitudes_2017_2026.csv"
+    csv_path = ROOT / "data" / "clean_data" / "solicitudes" / "clean_solicitudes_2017_2026.csv"
+    output_path = ROOT / "data" / "viz_data" / "ngrams_por_ministro.csv"
+
     all_minister_results = []
     todos_los_ministros = set()
     for lista in INTEGRACION_POR_ANIO.values():
         todos_los_ministros.update(lista)
     
-    print(f"Begin ngrams counts by minister by year")
+    print(f"Begin ngrams counts by minister by year in file {csv_path.name}")
 
     for ministro in sorted(todos_los_ministros):
         # 10 ngrams per minister 
@@ -21,16 +34,18 @@ def main():
                 dato['minister'] = ministro
                 all_minister_results.append(dato)
         else:
-            print(f"No se encontraron temas para {ministro}")
+            print(f"Nothing found for this minister {ministro}")
 
     # file for streamlit 
     if all_minister_results:
         output_df = pd.DataFrame(all_minister_results)
         output_df = output_df[['year', 'minister', 'ngram', 'count', 'score']]
-        output_df.to_csv("ngrams_por_ministro.csv", index=False)
-        print("\n Archivo 'ngrams_por_ministro.csv")
+        output_df.to_csv(output_path, index=False)
+        print("\n file generated: 'ngrams_por_ministro.csv")
     else:
-        print("\n No se generaron datos, error con archivo")
+        print("\n Data not generated, error with input file")
 
 if __name__ == "__main__":
     main()
+
+# uv run src/cleaning_and_processing/solicitudes/ngrams_datos.py
